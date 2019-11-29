546ad568d8c0444cecf8bd54ee5827dbaad8b34b
Jarong Hsu <jaronghsu@gmail.com>
2019-11-29 14:50:54 -0500

    New add

---
+++ PageObjects/Homepage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSECU
+{
+    public class Homepage 
+    {
+        IWebDriver wd;
+        
+        public Homepage(IWebDriver driver)
+        {
+            this.wd = driver;
+        }
+        public IWebElement GetUserName()
+        {
+            IWebElement userName = wd.FindElement(By.Name("username"));
+            return userName;
+        }
+        public IWebElement GetSignInButton()
+        {
+            IWebElement signInButton = wd.FindElement(By.CssSelector("input[type=submit]"));
+            return signInButton;
+        }
+        public void SetUserName(string sValue)
+        {
+            this.GetUserName().SendKeys(sValue);   
+        }
+    }
+}
---
+++ TCs/VerifyIncorrectNameErrorMsg.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace WSECU
+{
+    class VerifyIncorrectNameErrorMsg
+    {
+        private IWebDriver wd;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            Driver.initChromeBrowser();
+            wd = Driver.browserWin;
+        }
+        public static IEnumerable<string> GetTestData()
+        {
+            Utilities util = new Utilities();
+            return util.LoadXMLdata();
+        }
+        
+        [Test, TestCaseSource("GetTestData")]
+        public void VerifyErrorMsg(string x)
+        {
+            // Go to homepage
+            wd.Navigate().GoToUrl("https://wsecu.org/");
+            
+            Homepage objHome = new Homepage(wd);
+            SignIn objSignIn = new SignIn(wd);
+            
+            // Enter username and click sign in button
+            objHome.SetUserName(x);
+            objHome.GetSignInButton().Click();
+            // Verify it is redirected to sign in page
+            Assert.AreEqual("Sign in to Online Banking", wd.Title.Trim());
+
+            // Enter password and click sign in
+            objSignIn.SetPassword("fakepwd");
+            objSignIn.GetSignInButton().Click();
+
+            // Verify error message of incorrect username
+            String errorMsg = wd.FindElement(By.XPath("//div[@role='alert']/div/div")).Text;
+            Assert.AreEqual("Sorry, incorrect username.", errorMsg);
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            wd.Close();
+        }
+    }
+}
